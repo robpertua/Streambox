0db93d9ab388ac0feeabd2933c67487c56ff4c62
robpertua <robpertua@gmail.com>
2025-08-24 06:22:51 +0800

    Update app.js

--- app.js
+++ app.js
@@ -611,11 +611,6 @@ async function renderWatch(params) {
                ${controls}
              </div>
              <div class="controls-secondary">
-               <button class="btn" id="fsBtn">
-                 <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                   <path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3"/>
-                 </svg>
-               </button>
                <button class="btn" id="watchlistBtn">
                  ${inWatchlist(data) ? '★ In Watchlist' : '☆ Add to Watchlist'}
                </button>
